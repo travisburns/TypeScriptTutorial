f66053534f8164d64caedcbc243405fb35f0d355
Travis <travisjb0@gmail.com>
2023-08-06 00:53:39 -0700

    More on Variable types in typescript. On tupple type

--- main.ts
+++ main.ts
@@ -2,9 +2,29 @@ export {}
 let message = 'Wlecome back'
 console.log(message);
 
+
+
+
+
 let x = 10;
 const y = 20;
 
 let sum;
 const title = 'TypeScript Tutorial';
 
+let isBeginner: boolean = true;
+let total: number = 0;
+let name: string = 'Vishwas';
+
+
+let sentence: string = ` My name is ${name}
+I am a beginner in Typescript`
+
+console.log(sentence)
+
+
+let n: null = null;
+let u: undefined = undefined;
+
+let isNew: boolean = null;
+let myName: string = undefined;
