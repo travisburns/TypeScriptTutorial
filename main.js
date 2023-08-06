afbe98ba66a736720f6d278dc625111f3b6097e8
Travis <travisjb0@gmail.com>
2023-08-06 00:25:21 -0700

    Added and learned Basic Variable Declarations and how typescript handles them.

--- main.js
+++ main.js
@@ -2,3 +2,7 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 var message = 'Wlecome back';
 console.log(message);
+var x = 10;
+var y = 20;
+var sum;
+var title = 'TypeScript Tutorial';
--- main.ts
+++ main.ts
@@ -1,3 +1,10 @@
 export {}
 let message = 'Wlecome back'
 console.log(message);
+
+let x = 10;
+const y = 20;
+
+let sum;
+const title = 'TypeScript Tutorial';
+
