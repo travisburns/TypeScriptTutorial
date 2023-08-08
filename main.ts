0d43919fd768e91483af574a7106a355444433f3
Travis <travisjb0@gmail.com>
2023-08-07 17:17:43 -0700

    Learned about class inheretence and just classes in general with Typescript

--- main.ts
+++ main.ts
@@ -86,3 +86,36 @@ let p = {
 
 
 fullName(p);
+
+class Employee {
+    employeeName: string;
+
+    constructor(name: string) {
+        this.employeeName = name;
+    }
+
+    greet() {
+        console.log(`Good Morning ${this.employeeName}`)
+    }
+
+}
+
+let emp1 = new Employee('Vishwas');
+console.log(emp1.employeeName);
+emp1.greet();
+
+class Manager extends Employee {
+    constructor(managerName: string) {
+        super(managerName);
+    }
+
+    delegateWork() {
+        console.log(`Manager delegating Tasks`)
+    }
+
+}
+
+let m1 = new Manager(`Bruce`);
+m1.delegateWork();
+m1.greet();
+console.log(m1.employeeName);
